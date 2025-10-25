88f9663731c73a40ed12de4f82a62df8e36fc8cf
TienTheKnightMare <tienvm886@gmail.com>
2025-10-25 11:07:36 +0700

    Create baseview command

---
+++ QuanLyThuVien/ViewModels/ViewModelCommand.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Windows.Input;
+
+namespace QuanLyThuVien.ViewModels
+{
+    public class ViewModelCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+        public ViewModelCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _execute = executeAction;
+            _canExecute = null;
+        }
+
+        //Events
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
