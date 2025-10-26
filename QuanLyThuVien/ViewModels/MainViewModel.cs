0c14681357b546b1dd120ec1aa00fc51f8de9763
TienTheKnightMare <tienvm886@gmail.com>
2025-10-26 23:41:31 +0700

    update the DI logic, using *FrameworkElement

--- QuanLyThuVien/ViewModels/MainViewModel.cs
+++ QuanLyThuVien/ViewModels/MainViewModel.cs
@@ -1,14 +1,19 @@
 ﻿
+using Microsoft.Extensions.DependencyInjection;
 using QuanLyThuVien.ViewModels;
+using QuanLyThuVien.Views;
 using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace QuanLyThuVien.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public MainViewModel()
+        private readonly IServiceProvider _serviceProvider;
+        public MainViewModel(IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
             ShowDashBoardView = new ViewModelCommand(ExecuteShowDashBoardView);
             ShowQuanLySinhVienView = new ViewModelCommand(ExecuteShowQuanLySinhVienView);
             ShowQuanLySachView = new ViewModelCommand(ExecuteShowQuanLySachView);
@@ -16,42 +21,7 @@ namespace QuanLyThuVien.ViewModels
             ExecuteShowDashBoardView(null);
         }
 
-        private void ExecuteShowMuonTraSachView(object obj)
-        {
-            IsAddButtonVisible = true;
-            CurrentChildView = new MuonTraSachViewModel();
-            Title = "Mượn trả sách";
-            Caption = "Quản lý các giao dịch mượn và trả sách";
-            ButtonName = "Thêm phiếu mượn";
-        }
-
-        private void ExecuteShowQuanLySachView(object obj)
-        {
-            IsAddButtonVisible = true;
-            CurrentChildView = new QuanLySachViewModel();
-            Title = "Quản Lý Sách";
-            Caption = "Quản lý thông tin đầu sách và bản sao trong thư viện";
-            ButtonName = "Thêm đầu sách mới";
-        }
-
-        private void ExecuteShowQuanLySinhVienView(object obj)
-        {
-            IsAddButtonVisible = true;
-            CurrentChildView = new QuanLySinhVienViewModel();
-            Title = "Quản Lý Sinh Viên";
-            Caption = "Quản lý thông tin và tài khoản sinh viên";
-            ButtonName = "Tạo tài khoản mới";
-        }
-
-        private void ExecuteShowDashBoardView(object obj)
-        {
-            IsAddButtonVisible = false;
-            CurrentChildView = new DashBoardViewModel();
-            Title = "Dashboard";
-            Caption = "Tổng quan hệ thống quản lý thư viện";
-        }
-
-        private ViewModelBase _currentChildView;
+        private FrameworkElement _currentChildView;
         private string _title;
         private string _caption;
         private string _buttonName = "";
@@ -70,7 +40,7 @@ namespace QuanLyThuVien.ViewModels
         }
 
 
-        public ViewModelBase CurrentChildView { get => _currentChildView; set { _currentChildView = value; OnPropertyChanged(nameof(CurrentChildView)); } }
+        public FrameworkElement CurrentChildView { get => _currentChildView; set { _currentChildView = value; OnPropertyChanged(nameof(CurrentChildView)); } }
         public string Title { get => _title; set { _title = value; OnPropertyChanged(nameof(Title)); } }
         public string Caption { get => _caption; set { _caption = value; OnPropertyChanged(nameof(Caption)); } }
         public string ButtonName { get => _buttonName; set { _buttonName = value; OnPropertyChanged(nameof(ButtonName)); } }
@@ -79,7 +49,39 @@ namespace QuanLyThuVien.ViewModels
         public ICommand ShowQuanLySinhVienView { get; }
         public ICommand ShowQuanLySachView { get; }
         public ICommand ShowMuonTraSachView { get; }
+        private void ExecuteShowMuonTraSachView(object obj)
+        {
+            IsAddButtonVisible = true;
+            CurrentChildView = _serviceProvider.GetRequiredService<MuonTraSachView>();
+            Title = "Mượn trả sách";
+            Caption = "Quản lý các giao dịch mượn và trả sách";
+            ButtonName = "Thêm phiếu mượn";
+        }
 
+        private void ExecuteShowQuanLySachView(object obj)
+        {
+            IsAddButtonVisible = true;
+            CurrentChildView = _serviceProvider.GetRequiredService<QuanLySachView>();
+            Title = "Quản Lý Sách";
+            Caption = "Quản lý thông tin đầu sách và bản sao trong thư viện";
+            ButtonName = "Thêm đầu sách mới";
+        }
 
+        private void ExecuteShowQuanLySinhVienView(object obj)
+        {
+            IsAddButtonVisible = true;
+            CurrentChildView = _serviceProvider.GetRequiredService<QuanLySinhVienView>();
+            Title = "Quản Lý Sinh Viên";
+            Caption = "Quản lý thông tin và tài khoản sinh viên";
+            ButtonName = "Tạo tài khoản mới";
+        }
+
+        private void ExecuteShowDashBoardView(object obj)
+        {
+            IsAddButtonVisible = false;
+            CurrentChildView = _serviceProvider.GetRequiredService<DashBoardView>();
+            Title = "Dashboard";
+            Caption = "Tổng quan hệ thống quản lý thư viện";
+        }
     }
 }
