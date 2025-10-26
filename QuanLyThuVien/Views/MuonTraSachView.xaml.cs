2f6b42c5b5dda78fcc8011e566fcf823f563791e
TienTheKnightMare <tienvm886@gmail.com>
2025-10-26 23:42:35 +0700

    Binding datacontext to ViewModel

--- QuanLyThuVien/Views/MuonTraSachView.xaml.cs
+++ QuanLyThuVien/Views/MuonTraSachView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyThuVien.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,10 @@ namespace QuanLyThuVien.Views
     /// </summary>
     public partial class MuonTraSachView : UserControl
     {
-        public MuonTraSachView()
+        public MuonTraSachView(MuonTraSachViewModel vm)
         {
             InitializeComponent();
+            DataContext = vm;
         }
     }
 }
