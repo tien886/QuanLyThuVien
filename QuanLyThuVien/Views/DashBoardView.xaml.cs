256f4f4721fc5d3cb6a34bef825402c79a0fc1e6
TienTheKnightMare <tienvm886@gmail.com>
2025-10-26 09:23:39 +0700

    Update base dasboard view, no list of noti

--- QuanLyThuVien/Views/DashBoardView.xaml.cs
+++ QuanLyThuVien/Views/DashBoardView.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace QuanLyThuVien.Views
 {
     /// <summary>
-    /// Interaction logic for DashBoard.xaml
+    /// Interaction logic for DashBoardView.xaml
     /// </summary>
-    public partial class DashBoardView : Window
+    public partial class DashBoardView : UserControl
     {
-        public DashBoardView()  
+        public DashBoardView()
         {
             InitializeComponent();
         }
