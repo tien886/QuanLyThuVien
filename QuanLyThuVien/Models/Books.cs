ae3f6249d3ed495af828bc980219396df81ed7f2
TienTheKnightMare <tienvm886@gmail.com>
2025-10-27 08:40:16 +0700

    Add Book relation

---
+++ QuanLyThuVien/Models/Books.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyThuVien.Models
+{
+    public class Books
+    {
+        [Key]
+        public int BookID { get; set; }
+        public string ISBN { get; set; }
+        public string title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationYear { get; set; }
+        public string Genre { get; set; }
+        public int CategoryID { get; set; }
+
+        // Navigation property
+        public BookCategories BookCategory { get; set; }
+        public ICollection<BookCopies> BookCopies { get; set; }
+    }
+}
