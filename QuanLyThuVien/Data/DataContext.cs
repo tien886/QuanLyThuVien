74af2a1ea496f172c968616833e5a9d6bcd79e7c
TienTheKnightMare <tienvm886@gmail.com>
2025-10-27 22:31:13 +0700

    fix unexpected bug

--- QuanLyThuVien/Data/DataContext.cs
+++ QuanLyThuVien/Data/DataContext.cs
@@ -3,6 +3,8 @@ using QuanLyThuVien.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
