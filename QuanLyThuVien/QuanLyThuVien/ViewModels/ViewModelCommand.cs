320343b983a1371a911764b045180ace4ba0d30e
TienTheKnightMare <tienvm886@gmail.com>
2025-10-28 15:41:09 +0700

    no word for this commit, forgive me

---
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/ViewModelCommand.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Windows.Input;
+
+namespace QuanLyThuVien.ViewModels
+{
+    public class ViewModelCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+        public ViewModelCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _execute = executeAction;
+            _canExecute = null;
+        }
+
+        //Events
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+    }
+}
