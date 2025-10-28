7c780d2288cfc4131f8b04b5ca0028dc0ce13bd1
TienTheKnightMare <tienvm886@gmail.com>
2025-10-28 15:36:58 +0700

    Seed data for books, bookcategories and bookcopies

---
+++ QuanLyThuVien/QuanLyThuVien/Helper/DataSeeder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyThuVien.Helper.SeedModule;
+using QuanLyThuVien.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Helper
+{
+    public class DataSeeder
+    {
+        public static void SeedData(ModelBuilder modelBuilder)
+        {
+            BookSeeder.SeedBook(modelBuilder);
+            Debug.WriteLine("Seed books succesfullly");
+            BookCategorySeeder.SeedBookCategory(modelBuilder);
+            Debug.WriteLine("Seed bookcategory succesfullly");
+            BookCopySeeder.SeedBookCopy(modelBuilder);
+            Debug.WriteLine("Seed bookcopies succesfullly");
+        }
+    }
+}
