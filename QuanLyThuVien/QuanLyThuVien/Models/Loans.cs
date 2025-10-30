bc33bcc64d5bdbac65a4f92ae258a8946c2b6190
TienTheKnightMare <tienvm886@gmail.com>
2025-10-30 21:35:18 +0700

    two new models

---
+++ QuanLyThuVien/QuanLyThuVien/Models/Loans.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyThuVien.Models
+{
+    public class Loans
+    {
+        [Key]
+        public int LoanID { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string LoanStatus { get; set; }
+        // FK
+        public string CopyID { get; set; }
+        public int StudentID { get; set; }
+        // Navigation property
+        public BookCopies BookCopy { get; set; }
+        public Students Student { get; set; }
+    }
+}
