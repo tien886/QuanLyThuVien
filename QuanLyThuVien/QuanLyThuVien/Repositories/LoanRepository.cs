500e6e78ddb51b67b58091b08facb22bf5845452
TienTheKnightMare <tienvm886@gmail.com>
2025-10-31 23:31:47 +0700

    add loans service and repo

---
+++ QuanLyThuVien/QuanLyThuVien/Repositories/LoanRepository.cs
@@ -0,0 +1,13 @@
+﻿using QuanLyThuVien.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Repositories
+{
+    public class LoanRepository : ILoanService
+    {
+    }
+}
