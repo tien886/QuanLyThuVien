22175ddfe15bcbf95441a82ae9e55af2bc5d08f1
TienTheKnightMare <tienvm886@gmail.com>
2025-11-04 23:29:29 +0700

    Add bookdetailandcopy view [DONE]

---
+++ QuanLyThuVien/QuanLyThuVien/Views/QuanLySachPopup/BookDetailAndCopyPopup.xaml.cs
@@ -0,0 +1,17 @@
+﻿using QuanLyThuVien.ViewModels.QuanLySach;
+using System.Windows;
+namespace QuanLyThuVien.ViewModels.QuanLySachPopup
+{
+    /// <summary>
+    /// Interaction logic for BookDetailAndCopyPopup.xaml
+    /// </summary>
+    public partial class BookDetailAndCopyPopup : Window
+    {
+        public BookDetailAndCopyPopup(BookDetailAndCopyViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+    }
+}
