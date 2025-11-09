9642202d5f3d029cb4a4427504fcbc338da5c1cd
TienTheKnightMare <tienvm886@gmail.com>
2025-11-09 19:18:57 +0700

    Enhance book management popups and data bindings
    - Updated `BookDetailAndCopyPopup` to use `BookDetailAndCopyViewModel`.
    - Created `SuaBookHeadPopup.xaml` and its code-behind for editing book details.
    - Created `ThemBooKHeadPopup.xaml` and its code-behind for adding new book details.
    - Modified `ThemBookCopyPopup.xaml` to change `TextBox` binding and update button commands.
    - Updated `ThemBookCopyPopup.xaml.cs` to use `ThemBookCopyViewModel` for data context.

--- QuanLyThuVien/QuanLyThuVien/Views/QuanLySachPopup/ThemBookCopyPopup.xaml.cs
+++ QuanLyThuVien/QuanLyThuVien/Views/QuanLySachPopup/ThemBookCopyPopup.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using QuanLyThuVien.ViewModels.QuanLySach;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace QuanLyThuVien.Views.QuanLySachPopup
 {
@@ -19,9 +9,12 @@ namespace QuanLyThuVien.Views.QuanLySachPopup
     /// </summary>
     public partial class ThemBookCopyPopup : Window
     {
-        public ThemBookCopyPopup()
+        public ThemBookCopyPopup(ThemBookCopyViewModel vm)
         {
             InitializeComponent();
+            DataContext = vm;
         }
+
+        
     }
 }
