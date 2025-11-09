226e6b65b8d64b1f949b959ccb210fec0ea0e5e5
TienTheKnightMare <tienvm886@gmail.com>
2025-11-09 19:18:44 +0700

    Enhance book management view models
    - Updated `BookDetailAndCopyViewModel` to check `DataContext` before setting the current book.
    - Created `SuaBookHeadViewModel` and `ThemBookHeadViewModel` classes with no additional functionality.
    - Refactored `ThemBookCopyViewModel` to include new properties and methods for managing book copies, including setting the current book and closing the popup.

--- QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySach/ThemBookCopyViewModel.cs
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySach/ThemBookCopyViewModel.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using QuanLyThuVien.Models;
+using QuanLyThuVien.Services;
+using QuanLyThuVien.ViewModels.QuanLySachPopup;
+using QuanLyThuVien.Views.QuanLySachPopup;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+
 
 namespace QuanLyThuVien.ViewModels.QuanLySach
 {
-    public partial class ThemBookCopyViewModel
+    public partial class ThemBookCopyViewModel : ObservableObject
     {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IBookCopyService _bookCopyService;
+        private Books currentBook;
+        [ObservableProperty]
+        private string location;
+        [ObservableProperty]
+        private string copyID;
+        public ThemBookCopyViewModel(
+            IServiceProvider serviceProvider,
+            IBookCopyService bookCopyService
+            )
+        {
+            _serviceProvider = serviceProvider;
+            _bookCopyService = bookCopyService;
+        }
+        public async Task SetCurrentBook(Books book)
+        {
+            currentBook = book;
+            if(currentBook == null)
+            {
+                Debug.WriteLine("Book transmission failed");
+            }
+            else
+            {
+                CopyID = await _bookCopyService.GetNextAvailableBookCopyID();
+                Debug.WriteLine($"add a {currentBook.Title} copies");
+            }
+        }
+        [RelayCommand]
+        public async Task AddBookCopies()
+        {
+            BookCopies bookCopies = new BookCopies
+            {
+                CopyID = await _bookCopyService.GetNextAvailableBookCopyID(),
+                BookID = currentBook.BookID,
+                Status = "1",
+                Location = Location,
+                DateAdded = DateTime.Now
+            };
+            await _bookCopyService.AddBookCopiesAsync(bookCopies);
+            await ClosePopup();
+        }
+        [RelayCommand]
+        public async Task ClosePopup()
+        {
+            var window = Application.Current.Windows
+             .OfType<ThemBookCopyPopup>()
+             .FirstOrDefault(w => w.IsActive);
+
+            window?.Close();
+
+        }
     }
 }
