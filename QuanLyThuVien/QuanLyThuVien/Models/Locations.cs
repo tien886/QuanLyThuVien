654ded64797086137a45e739b603deccd98e5f59
Tientryhard <tienvm886@gmail.com>
2025-11-18 14:57:57 +0700

    model modification --- - Add locations - Add ref of loc to bookcopies - Remove discription from book categories

---
+++ QuanLyThuVien/QuanLyThuVien/Models/Locations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Models
+{
+    public class Locations
+    {
+        [Key]
+        public int LocationID { get; set; }
+        public string LocName { get; set; }
+        // navigation property
+        public ICollection<BookCopies> BookCopies { get; set; }
+    }
+}
