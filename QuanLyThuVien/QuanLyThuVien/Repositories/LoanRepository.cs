7a815b52768f99eacaf469f4f99d0435725c0a52
Tientryhard <tienvm886@gmail.com>
2025-11-22 16:07:22 +0700

    Refactor loan management with repository and interface
    Updated LoanRepository to implement loan retrieval methods and added necessary using directives. Converted ILoanService from a class to an interface, adding method signatures for loan operations.

--- QuanLyThuVien/QuanLyThuVien/Repositories/LoanRepository.cs
+++ QuanLyThuVien/QuanLyThuVien/Repositories/LoanRepository.cs
@@ -1,6 +1,10 @@
-﻿using QuanLyThuVien.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLyThuVien.Data;
+using QuanLyThuVien.Models;
+using QuanLyThuVien.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,37 @@ namespace QuanLyThuVien.Repositories
 {
     public class LoanRepository : ILoanService
     {
+        private readonly DataContext _dataContext;
+        public LoanRepository(DataContext dataContext) {
+            _dataContext = dataContext;
+        }
+        public async Task<int> GetDangMuon()
+        {
+            return await _dataContext.Loans.CountAsync(l => l.LoanStatus == "0");
+        }
+
+        public async Task<int> GetQuaHan()
+        {
+            return await _dataContext.Loans.CountAsync(l => l.LoanStatus == "0" && l.ReturnDate > l.DueDate);
+        }
+        public async Task<int> GetDaTraTheoThang(DateTime present)
+        {
+            int month = present.Month;
+            int year = present.Year;
+
+            return await _dataContext.Loans
+                .CountAsync(l =>
+                    l.LoanStatus == "1" &&
+                    l.ReturnDate.Month == month &&
+                    l.ReturnDate.Year == year
+                );
+        }
+        public async Task<IEnumerable<Loans>> GetAllLoansAsync()
+        {
+            return await _dataContext.Loans
+                .Include(s => s.Student)
+                .Include(b => b.BookCopy.Book)
+                .ToListAsync();
+        }
     }
 }
