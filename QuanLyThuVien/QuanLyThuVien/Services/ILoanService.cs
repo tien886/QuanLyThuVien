3ce0a0b601b3196f14d386b8b5fdb01fab8a2a65
ducchinh <hoangducchinh730@gmail.com>
2025-11-24 12:10:51 +0700

    add StudentLoanStats by id

--- QuanLyThuVien/QuanLyThuVien/Services/ILoanService.cs
+++ QuanLyThuVien/QuanLyThuVien/Services/ILoanService.cs
@@ -8,11 +8,20 @@ using System.Threading.Tasks;
 
 namespace QuanLyThuVien.Services
 {
+    public class StudentLoanStats
+    {
+        public int TotalBorrowed { get; set; } 
+        public int CurrentlyBorrowed { get; set; }
+        public int Overdue { get; set; }
+    }
+
     public interface ILoanService
     {
         Task<int> GetDangMuon();
         Task<int> GetQuaHan();
         Task<int> GetDaTraTheoThang(DateTime present);
         Task<IEnumerable<Loans>> GetAllLoansAsync();
+
+        Task<StudentLoanStats> GetLoanStatsByStudentIdAsync(int studentId);
     }
 }
