2e46757c257bd7653b4d1890b37a7655d9d3be39
ducchinh <hoangducchinh730@gmail.com>
2025-11-24 12:07:24 +0700

    Add FacultyRepository.cs and IFacultyService.cs

---
+++ QuanLyThuVien/QuanLyThuVien/Services/IFacultyService.cs
@@ -0,0 +1,14 @@
+﻿using QuanLyThuVien.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.Services
+{
+    public interface IFacultyService
+    {
+        Task<IEnumerable<Faculties>> GetAllFacultiesAsync();
+    }
+}
