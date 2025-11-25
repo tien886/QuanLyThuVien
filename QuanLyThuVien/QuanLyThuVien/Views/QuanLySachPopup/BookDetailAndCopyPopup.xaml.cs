811888bf9a3949a10a6c41f903bf6d4b85c785c9
Tientryhard <tienvm886@gmail.com>
2025-11-25 16:09:41 +0700

    finish the UI of QuanLySach related branch

--- QuanLyThuVien/QuanLyThuVien/Views/QuanLySachPopup/BookDetailAndCopyPopup.xaml.cs
+++ QuanLyThuVien/QuanLyThuVien/Views/QuanLySachPopup/BookDetailAndCopyPopup.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using QuanLyThuVien.ViewModels.QuanLySach;
 using System.Windows;
+using System.Windows.Controls;
 namespace QuanLyThuVien.ViewModels.QuanLySachPopup
 {
     /// <summary>
     /// Interaction logic for BookDetailAndCopyPopup.xaml
     /// </summary>
-    public partial class BookDetailAndCopyPopup : Window
+    public partial class BookDetailAndCopyPopup : UserControl
     {
         public BookDetailAndCopyPopup(BookDetailAndCopyViewModel vm)
         {
