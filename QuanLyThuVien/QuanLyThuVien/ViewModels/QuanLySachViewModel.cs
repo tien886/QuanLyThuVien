3b5fc99a478931f02d4d2bf98366f5205e2b200a
Tientryhard <tienvm886@gmail.com>
2025-11-27 23:28:52 +0700

    add pagination logic

--- QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySachViewModel.cs
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySachViewModel.cs
@@ -12,6 +12,7 @@ using QuanLyThuVien.Views;
 using QuanLyThuVien.Views.QuanLySachPopup;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
@@ -30,7 +31,27 @@ namespace QuanLyThuVien.ViewModels
             _bookService = bookService;
             _ = LoadPage();
         }
+        private async Task LoadCurrentPageAsync()
+        {
+            if (CurrentPage < 1 || CurrentPage > TotalPages)
+                return;
+            var books = await _bookService.GetBooksPage(CurrentPage - 1, PageSize);
+            BookList = new ObservableCollection<Books>(books);
+        }
+        private int PageSize = 15;
+        private const int WindowSize = 5;
 
+        [ObservableProperty]
+        private int currentPage;
+
+        [ObservableProperty]
+        private int totalPages;
+
+        [ObservableProperty] private int firstPage;
+        [ObservableProperty] private int secondPage;
+        [ObservableProperty] private int thirdPage;
+        [ObservableProperty] private int fourthPage;
+        [ObservableProperty] private int fifthPage;
         private string _searhBarText = "";
         public string SearchBarText
         {
@@ -51,12 +72,17 @@ namespace QuanLyThuVien.ViewModels
 
         [ObservableProperty]
         private bool available = false;
-
+        // Pagination
         public async Task LoadPage()
         {
-            BookList = new ObservableCollection<Books>(await _bookService.GetAllBooksAsync());
+            CurrentPage = 1;
+            TotalPages = await _bookService.GetTotalPages(PageSize);
+            UpdateWindow();
+            await LoadCurrentPageAsync();
+            Debug.WriteLine(CurrentPage);
+            Debug.WriteLine(TotalPages);
         }
-
+        
         public async Task SearchBookAsync(string hint)
         {
             try
@@ -79,6 +105,7 @@ namespace QuanLyThuVien.ViewModels
                 Debug.WriteLine($"Lỗi khi tìm kiếm: {ex.Message}");
             }
         }
+        
 
         [RelayCommand]
         private async Task DeleteBook(Books book)
@@ -123,6 +150,64 @@ namespace QuanLyThuVien.ViewModels
             {
                 await vm.LoadPage(book);
             }
+        }
+        [RelayCommand]
+        private void PageSelection(int pageNumber)
+        {
+            Debug.WriteLine(pageNumber);
+            if (pageNumber < 0 || pageNumber > TotalPages)
+                return;
+
+            CurrentPage = pageNumber;
+            UpdateWindow();
+            _ = LoadCurrentPageAsync();
+        }
+        [RelayCommand]
+        private async Task GoToPreviousPage()
+        {
+            if (CurrentPage > 0)
+            {
+                CurrentPage--;
+                UpdateWindow();
+
+                var books = await _bookService.GetBooksPage(CurrentPage, PageSize);
+                BookList = new ObservableCollection<Books>(books);
+            }
+        }
+
+        [RelayCommand]
+        private async Task GoToNextPage()
+        {
+            if (CurrentPage < TotalPages)
+            {
+                CurrentPage++;
+                UpdateWindow();
+
+                var books = await _bookService.GetBooksPage(CurrentPage, PageSize);
+                BookList = new ObservableCollection<Books>(books);
+            }
+        }
+        private void UpdateWindow()
+        {
+            if (TotalPages <= 1)
+                return;
+
+            int start = CurrentPage - WindowSize / 2;
+            if (start < 1)
+                start = 1;
+
+            int end = start + WindowSize - 1;
+            if (end > TotalPages)
+            {
+                end = TotalPages;
+                start = Math.Max(1, end - WindowSize + 1);
+            }
+
+            FirstPage = start;
+            SecondPage = start + 1 <= end ? start + 1 : 0;
+            ThirdPage = start + 2 <= end ? start + 2 : 0;
+            FourthPage = start + 3 <= end ? start + 3 : 0;
+            FifthPage = start + 4 <= end ? start + 4 : 0;
         }
     } 
 }
