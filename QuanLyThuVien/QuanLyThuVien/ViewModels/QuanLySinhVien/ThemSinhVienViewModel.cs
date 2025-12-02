b0e70882b5bca29dc016c7ca7bc04e44dd16be0d
Tientryhard <tienvm886@gmail.com>
2025-12-02 10:06:07 +0700

    initialize viewmodel for two emerging UI

---
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySinhVien/ThemSinhVienViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuVien.ViewModels.QuanLySinhVien
+{
+    public partial class ThemSinhVienViewModel : ObservableObject
+    {
+    }
+}
