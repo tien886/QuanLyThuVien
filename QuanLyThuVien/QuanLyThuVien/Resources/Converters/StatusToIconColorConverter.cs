26a4c23f1010607da2d36510e7a7a7d129cb940f
ducchinh <hoangducchinh730@gmail.com>
2025-12-03 08:34:30 +0700

    Fix the bug that the ban/unban toggle update the UI but not change the database. Change the Active -> 1, Disable -> 0.

--- QuanLyThuVien/QuanLyThuVien/Resources/Converters/StatusToIconColorConverter.cs
+++ QuanLyThuVien/QuanLyThuVien/Resources/Converters/StatusToIconColorConverter.cs
@@ -1,6 +1,6 @@
 ﻿using System.Globalization;
 using System.Windows.Data;
-using System.Windows.Media; // <-- Phải là System.Windows.Media, không phải System.Drawing
+using System.Windows.Media;
 
 namespace QuanLyThuVien.Resources.Converters
 {
@@ -9,7 +9,7 @@ namespace QuanLyThuVien.Resources.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string status = value?.ToString()?.Trim();
-            if (string.Equals(status, "Active", StringComparison.OrdinalIgnoreCase))
+            if (string.Equals(status, "1", StringComparison.OrdinalIgnoreCase))
             {
                 return Brushes.Red; 
             }
