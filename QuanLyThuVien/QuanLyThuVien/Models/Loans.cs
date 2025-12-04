0cec1baf27b671b0fca2895443f33f7f53884206
Tientryhard <tienvm886@gmail.com>
2025-12-04 22:48:15 +0700

    datetime can be null

--- QuanLyThuVien/QuanLyThuVien/Models/Loans.cs
+++ QuanLyThuVien/QuanLyThuVien/Models/Loans.cs
@@ -6,13 +6,13 @@ namespace QuanLyThuVien.Models
     {
         [Key]
         public int LoanID { get; set; }
-        public DateTime LoanDate { get; set; }
-        public DateTime DueDate { get; set; }
+        public DateTime? LoanDate { get; set; }
+        public DateTime? DueDate { get; set; }
         public DateTime? ReturnDate { get; set; }
         public string LoanStatus { get; set; }
         // FK
         public string CopyID { get; set; }
-        public int StudentID { get; set; }
+        public int? StudentID { get; set; }
         // Navigation property
         public BookCopies BookCopy { get; set; }
         public Students Student { get; set; }
