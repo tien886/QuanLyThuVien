0856f530a2a500a46c5f22ecd4b814c5041da892
Tientryhard <tienvm886@gmail.com>
2025-12-06 00:44:26 +0700

    remove the return logic and exchange to a new UI as i added before

---
+++ QuanLyThuVien/QuanLyThuVien/Views/MuonTraSachPopup/NhanTraSachPopup.xaml.cs
@@ -0,0 +1,30 @@
+﻿using QuanLyThuVien.ViewModels.MuonTraSach;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyThuVien.Views.MuonTraSachPopup
+{
+    /// <summary>
+    /// Interaction logic for NhanTraSachPopup.xaml
+    /// </summary>
+    public partial class NhanTraSachPopup : UserControl
+    {
+        public NhanTraSachPopup(NhanTraSachViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
