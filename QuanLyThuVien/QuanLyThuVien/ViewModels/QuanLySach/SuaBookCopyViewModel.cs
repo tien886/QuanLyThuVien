f5fff327fd0b8a8d111ec9c4fc74e09d6f0599d4
Tientryhard <tienvm886@gmail.com>
2025-12-06 00:37:12 +0700

    Add SuaBookCopyView and ViewModel

---
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySach/SuaBookCopyViewModel.cs
@@ -0,0 +1,69 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using QuanLyThuVien.Models;
+using QuanLyThuVien.Services;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+
+
+namespace QuanLyThuVien.ViewModels.QuanLySach
+{
+    public partial class SuaBookCopyViewModel : ObservableObject
+    {
+        private readonly IBookCopyService _bookCopyService;
+        private readonly ILocationService _locationService;
+        private BookCopies currentBookCopy;
+        [ObservableProperty]
+        private Locations locationSelected;
+        [ObservableProperty]
+        private ObservableCollection<Locations> locations;
+        [ObservableProperty]
+        private string copyID;
+        public SuaBookCopyViewModel(
+            IBookCopyService bookCopyService,
+            ILocationService locationService
+            )
+        {
+            _bookCopyService = bookCopyService;
+            _locationService = locationService;
+        }
+        public async Task SetCurrentBook(BookCopies bookCopy)
+        {
+            var locs = await _locationService.GetAllLocationsAsync();
+            Locations = new ObservableCollection<Locations>(locs);
+            currentBookCopy = bookCopy;
+            LocationSelected = await _bookCopyService.GetLocationByBookCopyID(CopyID);
+            Debug.WriteLine(LocationSelected.LocName);
+            if (currentBookCopy == null)
+            {
+                Debug.WriteLine("Book transmission failed");
+            }
+            else
+            {
+                CopyID = await _bookCopyService.GetNextAvailableBookCopyID();
+            }
+        }
+        [RelayCommand]
+        public async Task SaveBookCopies()
+        {
+            BookCopies bookCopies = new BookCopies
+            {
+                CopyID = await _bookCopyService.GetNextAvailableBookCopyID(),
+                BookID = currentBookCopy.BookID,
+                Status = "1",
+                LocationID = LocationSelected.LocationID,
+                DateAdded = DateTime.Now
+            };
+            await _bookCopyService.UpdateCopiesAsync(bookCopies);
+            MessageBox.Show("Sửa bản sao thành công!", "Thông báo");
+            await ClosePopup();
+        }
+        [RelayCommand]
+        public async Task ClosePopup()
+        {
+            WeakReferenceMessenger.Default.Send(new OpenDialogMessage(null));
+        }
+    }
+}
---
+++ QuanLyThuVien/QuanLyThuVien/Views/QuanLySachPopup/SuaBookCopyPopup.xaml.cs
@@ -0,0 +1,30 @@
+﻿using QuanLyThuVien.ViewModels.QuanLySach;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyThuVien.Views.QuanLySachPopup
+{
+    /// <summary>
+    /// Interaction logic for SuaBookCopyPopup.xaml
+    /// </summary>
+    public partial class SuaBookCopyPopup : UserControl
+    {
+        public SuaBookCopyPopup(SuaBookCopyViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
