05ab9d3cd2f7fad4cf5f6ea389a5d6f178c563f6
Tientryhard <tienvm886@gmail.com>
2025-12-05 11:56:37 +0700

    finish add phieu muon binding logic

--- QuanLyThuVien/QuanLyThuVien/ViewModels/MuonTraSach/ThemPhieuMuonViewModel.cs
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/MuonTraSach/ThemPhieuMuonViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using QuanLyThuVien.Models;
 using QuanLyThuVien.Services;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace QuanLyThuVien.ViewModels.MuonTraSach
 {
@@ -10,19 +11,30 @@ namespace QuanLyThuVien.ViewModels.MuonTraSach
     {
         private readonly IStudentService _studentService;
         private readonly IBookCopyService _bookCopyService;
+        private readonly IBookCategoryService _bookCategoryService;
         private readonly ILoanService _loanService;
+        private readonly IGenreService _genreService;
         public ThemPhieuMuonViewModel(
             IStudentService studentService,
             IBookCopyService bookCopyService,
-            ILoanService loanService
+            ILoanService loanService,
+            IGenreService genreService,
+            IBookCategoryService bookCategoryService
             )
         {
             _studentService = studentService;
             _bookCopyService = bookCopyService;
             _loanService = loanService;
+            _genreService = genreService;
+            _bookCategoryService = bookCategoryService;
+            LoadPage();
         }
         [ObservableProperty]
         private int? studentId;
+        [ObservableProperty]
+        private string? categoryName;
+        [ObservableProperty]
+        private string? genreName;
         [ObservableProperty]
         private string? studentName;
         [ObservableProperty]
@@ -33,6 +45,7 @@ namespace QuanLyThuVien.ViewModels.MuonTraSach
         private DateTime? loandate;
         [ObservableProperty]
         private DateTime? duedate;
+        
 
         [RelayCommand]
         public async Task CreateLoan()
@@ -49,6 +62,11 @@ namespace QuanLyThuVien.ViewModels.MuonTraSach
             };
             await _loanService.AddLoan(loans);
         }
+        private void LoadPage()
+        {
+            Loandate = DateTime.Now;
+            bookCopyID = "CP";
+        }
         partial void OnStudentIdChanged(int? value)
         {
             if (value.HasValue)
@@ -56,16 +74,27 @@ namespace QuanLyThuVien.ViewModels.MuonTraSach
             else
                 StudentName = string.Empty;
         }
-
-        // When BookCopyCode changes, try to load book copy
         partial void OnBookCopyIDChanged(string? value)
         {
-            if (!string.IsNullOrWhiteSpace(value) && value.Length < 9)
-                _ = LoadBookCopyAsync(value);
-            else
-                BookTitle = string.Empty;
+            _ = OnBookCopyIDChangedAsync(value);
         }
-
+        private async Task OnBookCopyIDChangedAsync(string? value)
+        {
+            if (!string.IsNullOrWhiteSpace(value) && value.Length == 8)
+            {
+                await LoadBookCopyAsync(value);
+                var copy = await _bookCopyService.GetBookCopiesByIDAsync(value);
+                if (copy == null)
+                    return;
+                var genre = await _genreService.GetGenreByID(copy.Book.GenreID);
+                int loanDays = genre.LoanDurationDays;
+                GenreName = genre.GenreName;
+                var category = await _bookCategoryService.GetBookCategoryByID(copy.Book.CategoryID);
+                CategoryName = category.CategoryName;
+                if (loanDays != 0) 
+                    Duedate = DateTime.Now.AddDays(loanDays);
+            }
+        }
         private async Task LoadStudentAsync(int id)
         {
             var student = await _studentService.GetStudentByIDAsync(id);
@@ -78,6 +107,6 @@ namespace QuanLyThuVien.ViewModels.MuonTraSach
             BookTitle = copy?.Book?.Title ?? "Không tìm thấy";
             Debug.WriteLine(BookTitle);
         }
-
+        
     }
 }
