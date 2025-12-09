8ac1e4ea3f16982f678bf8b58d4b354630d1078a
ducchinh <hoangducchinh730@gmail.com>
2025-12-09 15:54:49 +0700

    Add the UI for NhanTraSach

--- QuanLyThuVien/QuanLyThuVien/Views/MuonTraSachPopup/NhanTraSachPopup.xaml.cs
+++ QuanLyThuVien/QuanLyThuVien/Views/MuonTraSachPopup/NhanTraSachPopup.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using QuanLyThuVien.ViewModels.MuonTraSach;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace QuanLyThuVien.Views.MuonTraSachPopup
 {
@@ -21,10 +9,9 @@ namespace QuanLyThuVien.Views.MuonTraSachPopup
     /// </summary>
     public partial class NhanTraSachPopup : UserControl
     {
-        public NhanTraSachPopup(NhanTraSachViewModel vm)
+        public NhanTraSachPopup()
         {
             InitializeComponent();
-            DataContext = vm;
         }
     }
 }
