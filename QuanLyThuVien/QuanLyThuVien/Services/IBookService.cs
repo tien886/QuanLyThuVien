8023e1bd8325b26742a63f1a4d687074a7fc1e1e
ducchinh <hoangducchinh730@gmail.com>
2025-12-09 15:37:10 +0700

    Refactor the code for book repository and service. Rearrange and cluster the order of method

--- QuanLyThuVien/QuanLyThuVien/Services/IBookService.cs
+++ QuanLyThuVien/QuanLyThuVien/Services/IBookService.cs
@@ -10,8 +10,11 @@ namespace QuanLyThuVien.Services
         Task UpdateBookAsync(Books book);
         Task DeleteBookAsync (Books book);
         Task<int> GetTotalBooksAsync();
-        Task<IEnumerable<Books>> GetBooksPage(int offsetm, int size);
-        Task<int> GetTotalPages(int size);
+        //Task<IEnumerable<Books>> GetBooksPage(int offsetm, int size);
+        //Task<int> GetTotalPages(int size);
+
+        Task<int> GetTotalPages(int pageSize, string keyword = "");
+        Task<IEnumerable<Books>> GetBooksPage(int pageIndex, int pageSize, string keyword = "");
     }
 
 }
