47b140f0540f656da42ff176549887c715b04316
ducchinh <hoangducchinh730@gmail.com>
2025-12-09 15:41:55 +0700

    Refactor the code for student repository and service. Rearrange and cluster the order of method. Change the method GetNewReadersStatsAsync so it take two argument startDate and endDate. Remove MatchSearchKeyword method and GetAllStudentsAsync (because we use the paging mechanism so we only load the student for the current page)

--- QuanLyThuVien/QuanLyThuVien/Services/IStudentService.cs
+++ QuanLyThuVien/QuanLyThuVien/Services/IStudentService.cs
@@ -5,16 +5,29 @@ namespace QuanLyThuVien.Services
 {
     public interface IStudentService
     {
-        Task<IEnumerable<Students>> GetAllStudentsAsync(string? keyword = null);
+        // Thêm, xóa, sửa, ban/unban
+        //Task<IEnumerable<Students>> GetAllStudentsAsync(string? keyword = null);
         Task AddStudentAsync(Students s);
         Task UpdateStudentAsync(Students s);
-        Task<Students> GetStudentByIDAsync(int id);
         Task DeleteStudentAsync(int id);
-        Task BlockStudentAsync(int id);  
+        Task BlockStudentAsync(int id);
         Task UnblockStudentAsync(int id);
-        Task<IEnumerable<MonthlyReaderStats>> GetNewReadersStatsAsync();
+
+
+        // Lấy data cho sinh viên đăng kí trong các tháng (Dashboard)
+        Task<IEnumerable<MonthlyReaderStats>> GetNewReadersStatsAsync(DateTime startDate, DateTime endDate);
+
+
+        // Lây data cho hoạt động gần đây (Dashboard)
         Task<IEnumerable<Students>> GetRecentStudentsAsync(int count);
+
+
+        // Phân trang
         Task<IEnumerable<Students>> GetStudentsPage(int offsetm, int size, string? keyword);
         Task<int> GetTotalPages(int size, string? keyword = null);
+
+
+        // Lấy sinh viên (Thêm phiếu mượn)
+        Task<Students> GetStudentByIDAsync(int id);
     }
 }
