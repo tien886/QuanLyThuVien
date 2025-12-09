1d007e5c2d6c03e1346bf06b150d9b63f8a381a0
ducchinh <hoangducchinh730@gmail.com>
2025-12-09 16:27:56 +0700

    Refactor ThemBookHead popup ui

---
+++ QuanLyThuVien/QuanLyThuVien/Views/QuanLySachPopup/ThemBookHeadPopupView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+namespace QuanLyThuVien.Views.QuanLySachPopup
+{
+    /// <summary>
+    /// Interaction logic for ThemBooKHeadPopup.xaml
+    /// </summary>
+    public partial class ThemBooKHeadPopupView : UserControl
+    {
+        public ThemBooKHeadPopupView()
+        {
+            InitializeComponent();
+        }
+    }
+}
