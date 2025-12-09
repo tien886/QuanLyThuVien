ef71d504bbee36921e4d7f23b04ce999726de1b2
ducchinh <hoangducchinh730@gmail.com>
2025-12-09 16:15:00 +0700

    Change the way changed book head be updated to in the database and UI

--- QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySach/SuaBookHeadViewModel.cs
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySach/SuaBookHeadViewModel.cs
@@ -5,29 +5,20 @@ using QuanLyThuVien.Models;
 using QuanLyThuVien.Services;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace QuanLyThuVien.ViewModels.QuanLySach
 {
     public partial class SuaBookHeadViewModel : ObservableObject
     {
-        private readonly IServiceProvider _serviceProvider;
         private readonly IBookService _bookService;
         private readonly IBookCategoryService _bookCategoryService;
         private readonly IGenreService _genreService;
-        private Books currentBook;
-        public SuaBookHeadViewModel(
-            IServiceProvider serviceProvider,
-            IBookService bookService,
-            IBookCategoryService bookCategoryService,
-            IGenreService genreService
-            )
-        {
-            _serviceProvider = serviceProvider;
-            _bookService = bookService;
-            _bookCategoryService = bookCategoryService;
-            _genreService = genreService;
-        }
+        private readonly Books _originalBook;
+
+        // --- CÁC TRƯỜNG NHẬP LIỆU ---
         [ObservableProperty]
         private string tenSach;
         [ObservableProperty]
@@ -35,57 +26,125 @@ namespace QuanLyThuVien.ViewModels.QuanLySach
         [ObservableProperty]
         private string iSBN;
         [ObservableProperty]
-        private Genres loaiSachSelected;
-        [ObservableProperty]
-        private ObservableCollection<Genres> loaiSachs;
-        [ObservableProperty]
-        private BookCategories theLoaiSelected;
-        [ObservableProperty]
-        private ObservableCollection<BookCategories> theLoais;
-        [ObservableProperty]
         private int namXB;
         [ObservableProperty]
         private string nXB;
         [ObservableProperty]
         private string moTa;
-        public async Task LoadPage(Books book)
+
+        // ComboBox
+        [ObservableProperty]
+        private Genres _selectedGenre;
+        [ObservableProperty]
+        private ObservableCollection<Genres> genres = new();
+        [ObservableProperty]
+        private BookCategories _selectedCategory;
+        [ObservableProperty]
+        private ObservableCollection<BookCategories> categories = new();
+
+        public SuaBookHeadViewModel(
+            Books book,
+            IBookService bookService,
+            IBookCategoryService bookCategoryService,
+            IGenreService genreService
+            )
         {
-            currentBook = book;
-            var genres = await _genreService.GetAllGenresAsync();
-            LoaiSachs = new ObservableCollection<Genres>(genres);
-            var bookCategories = await _bookCategoryService.GetAllBookCategoriesAsync();
-            TheLoais = new ObservableCollection<BookCategories>(bookCategories);
-            TenSach = currentBook.Title;
-            TacGia = currentBook.Author;
-            ISBN = currentBook.ISBN;
-            LoaiSachSelected = LoaiSachs.FirstOrDefault(g => g.GenreID == currentBook.GenreID);
-            TheLoaiSelected = TheLoais.FirstOrDefault(c => c.CategoryID == currentBook.CategoryID);
-            Debug.WriteLine($"LoaiSachSelected: {LoaiSachSelected?.GenreName}, TheLoaiSelected : {TheLoaiSelected?.CategoryName}");
-            NamXB = currentBook.PublicationYear;
-            NXB = currentBook.Publisher;
-            MoTa = currentBook.Description;
+            _originalBook = book;
+            _bookService = bookService;
+            _bookCategoryService = bookCategoryService;
+            _genreService = genreService;
+
+            TenSach = book.Title;
+            TacGia = book.Author;
+            iSBN = book.ISBN;
+            NamXB = book.PublicationYear;
+            NXB = book.Publisher;
+            MoTa = book.Description;
+            SelectedGenre = book.Genre;
+            SelectedCategory = book.BookCategory;
+            LoadGenres();
+            LoadCategories();
         }
-        [RelayCommand]
-        public async Task SaveChanges()
+
+        private async void LoadGenres()
         {
-            currentBook.Title = TenSach;
-            currentBook.Author = TacGia;
-            currentBook.ISBN = ISBN;
-            currentBook.PublicationYear = NamXB;
-            currentBook.Description = MoTa;
-            currentBook.Publisher = NXB;
-            currentBook.BookCategory = TheLoaiSelected;
-            currentBook.CategoryID= TheLoaiSelected.CategoryID;
-            currentBook.Genre = LoaiSachSelected;
-            currentBook.GenreID = LoaiSachSelected.GenreID;
-            await _bookService.UpdateBookAsync(currentBook);
-            MessageBox.Show("Cập nhật thành công!", "Thông báo");
-            await ClosePopup();
+            var list = await _genreService.GetAllGenresAsync();
+            genres.Clear();
+            foreach ( var genre in list )
+            {
+                genres.Add(genre);
+            }
         }
+
+        private async void LoadCategories()
+        {
+            var list = await _bookCategoryService.GetAllBookCategoriesAsync();
+            categories.Clear();
+            foreach ( var category in list )
+            {
+                categories.Add(category);
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            return true;
+        }
+
         [RelayCommand]
-        public async Task ClosePopup()
+        public async Task Save()
+        {
+            if(!ValidateInput())
+            {
+                return;
+            }
+
+            // Cập nhật ngược lại vào model gốc (cái này là trong Book List - ObservableList)
+            _originalBook.Title = TenSach;
+            _originalBook.Author = TacGia;
+            _originalBook.ISBN = iSBN;
+            _originalBook.PublicationYear = NamXB;
+            _originalBook.Publisher = NXB;
+            _originalBook.Description = MoTa;
+            _originalBook.GenreID = _selectedGenre.GenreID;
+            _originalBook.CategoryID = _selectedCategory.CategoryID;
+
+            var selectedGenreObj = Genres.FirstOrDefault(g => g.GenreID == _selectedGenre.GenreID);
+            if (selectedGenreObj != null)
+            {
+                _originalBook.Genre = selectedGenreObj; 
+            }
+
+            var selectedCategoryObj = Categories.FirstOrDefault(c => c.CategoryID == _selectedCategory.CategoryID);
+            if (selectedCategoryObj != null)
+            {
+                _originalBook.BookCategory = selectedCategoryObj; 
+            }
+
+            try
+            {
+                // Lưu thay đổi vào Database và phát tín hiệu cập nhật UI 
+                await _bookService.UpdateBookAsync(_originalBook);
+                WeakReferenceMessenger.Default.Send(new BookUpdatedMessage(_originalBook));
+                MessageBox.Show("Cập nhật thành công!", "Thông báo");
+                ClosePopup();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi: {ex.Message}", "Lỗi");
+            }
+        }
+
+        [RelayCommand]
+        private void ClosePopup()
         {
             WeakReferenceMessenger.Default.Send(new OpenDialogMessage(null));
+        }
+
+        public class BookUpdatedMessage
+        {
+            public Books UpdatedBook { get; }
+            public BookUpdatedMessage(Books book) => UpdatedBook = book;
         }
     }
 }
