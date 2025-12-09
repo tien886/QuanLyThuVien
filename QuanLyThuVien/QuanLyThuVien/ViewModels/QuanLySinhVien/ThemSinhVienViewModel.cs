96688d1bb7f2eee38c31532ec8de45461c6e3e7c
ducchinh <hoangducchinh730@gmail.com>
2025-12-09 16:16:32 +0700

    Delete ThemSinhVienViewModel

--- QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySinhVien/ThemSinhVienViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace QuanLyThuVien.ViewModels.QuanLySinhVien
-{
-    public partial class ThemSinhVienViewModel : ObservableObject
-    {
-    }
-}
