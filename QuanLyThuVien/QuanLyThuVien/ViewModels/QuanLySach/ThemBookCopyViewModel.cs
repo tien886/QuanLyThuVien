f6e4fc19ad609bbb953b4397af14c8062099d828
ducchinh <hoangducchinh730@gmail.com>
2025-12-09 16:07:59 +0700

    Change the way added book be added to in the database. Don't have to send message to update ui because when we close the popup and open book detail popup the list will be reloaded.

--- QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySach/ThemBookCopyViewModel.cs
+++ QuanLyThuVien/QuanLyThuVien/ViewModels/QuanLySach/ThemBookCopyViewModel.cs
@@ -13,55 +13,104 @@ namespace QuanLyThuVien.ViewModels.QuanLySach
     {
         private readonly IBookCopyService _bookCopyService;
         private readonly ILocationService _locationService;
-        private Books currentBook;
+        private readonly Books _currentBook; // Lưu đầu sách cha để biết thêm bản sao cho sách nào
+
         [ObservableProperty]
-        private Locations locationSelected;
+        private Locations _selectedLocation;
+
         [ObservableProperty]
-        private ObservableCollection<Locations> locations;
+        private ObservableCollection<Locations> _locations = new();
+
         [ObservableProperty]
-        private string copyID;
+        private string _bookCopyID;
+
         public ThemBookCopyViewModel(
+            Books book,
             IBookCopyService bookCopyService,
-            ILocationService locationService
-            )
+            ILocationService locationService)
         {
+            _currentBook = book;
             _bookCopyService = bookCopyService;
             _locationService = locationService;
+
+            BookCopyID = _bookCopyService.GetNextAvailableBookCopyID().GetAwaiter().GetResult();
+            LoadData();
         }
-        public async Task SetCurrentBook(Books book)
+
+        private async void LoadLocations()
         {
-            var locs = await _locationService.GetAllLocationsAsync();
-            Locations = new ObservableCollection<Locations>(locs);
-            currentBook = book;
-            if(currentBook == null)
+            var list = await _locationService.GetAllLocationsAsync();
+            Locations.Clear();
+            foreach (var location in list)
             {
-                Debug.WriteLine("Book transmission failed");
+                Locations.Add(location);
             }
-            else
+
+            if (Locations.Count > 0)
             {
-                CopyID = await _bookCopyService.GetNextAvailableBookCopyID();
-                Debug.WriteLine($"add a {currentBook.Title} copies");
+                SelectedLocation = Locations[0];
             }
         }
+
+        // Load danh sách Location từ Database
+        private async void LoadData()
+        {
+            LoadLocations();
+
+            if (_currentBook != null)
+            {
+                BookCopyID = await _bookCopyService.GetNextAvailableBookCopyID();
+            }
+        }
+        
+
         [RelayCommand]
         public async Task AddBookCopies()
         {
-            BookCopies bookCopies = new BookCopies
+            if (SelectedLocation == null)
             {
-                CopyID = await _bookCopyService.GetNextAvailableBookCopyID(),
-                BookID = currentBook.BookID,
-                Status = "1",
-                LocationID = LocationSelected.LocationID,
+                MessageBox.Show("Vui lòng chọn vị trí!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (_currentBook == null) 
+                return;
+
+            var newBookCopy = new BookCopies
+            {
+                CopyID = BookCopyID, // ID đã sinh ở trên
+                BookID = _currentBook.BookID,
+                Status = "1", // Mặc định: 1 = Có sẵn
+                LocationID = SelectedLocation.LocationID,
                 DateAdded = DateTime.Now
             };
-            await _bookCopyService.AddBookCopiesAsync(bookCopies);
-            MessageBox.Show("Sửa bản sao thành công!", "Thông báo");
-            await ClosePopup();
+
+            try
+            {
+                // Lưu xuống Database
+                await _bookCopyService.AddBookCopiesAsync(newBookCopy);
+                // Trên DataGrid, bạn có cột "Vị trí" đang Binding vào Location.LocName. -> Nếu không có thể đéo có name của location đâu 
+                newBookCopy.Location = SelectedLocation;
+                //WeakReferenceMessenger.Default.Send(new BookCopyAddedMessage(newBookCopy));
+
+                MessageBox.Show("Thêm bản sao thành công!", "Thông báo");
+                ClosePopup();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi thêm: {ex.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
+
         [RelayCommand]
-        public async Task ClosePopup()
+        private void ClosePopup()
         {
             WeakReferenceMessenger.Default.Send(new OpenDialogMessage(null));
         }
+    }
+    public class BookCopyAddedMessage
+    {
+        public BookCopies NewBookCopy { get; }
+        public BookCopyAddedMessage(BookCopies bookCopy) => NewBookCopy = bookCopy;
     }
 }
