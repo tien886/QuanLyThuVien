a0c122728ee877e0d45725d7024a14a894d2ebe2
ducchinh <hoangducchinh730@gmail.com>
2025-12-14 16:06:54 +0700

    Add code ui for genre popup

---
+++ QuanLyThuVien/QuanLyThuVien/Views/QuanLyDanhMucPopup/GenrePopup.xaml.cs
@@ -0,0 +1,20 @@
+﻿using QuanLyThuVien.ViewModels;
+using QuanLyThuVien.ViewModels.QuanLyDanhMuc;
+using System.Windows.Controls;
+
+
+namespace QuanLyThuVien.Views.QuanLyDanhMucPopup
+{
+    /// <summary>
+    /// Interaction logic for QuanLySachView.xaml
+    /// </summary>
+    public partial class GenrePopup : UserControl
+    {
+        public GenrePopup()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
