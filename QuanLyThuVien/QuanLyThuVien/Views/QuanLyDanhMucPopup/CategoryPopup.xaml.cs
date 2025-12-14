ba7fc906b18d97dcb8d3c9e10a58e945283b9d8b
ducchinh <hoangducchinh730@gmail.com>
2025-12-14 16:06:19 +0700

    Add code ui for Category popup

---
+++ QuanLyThuVien/QuanLyThuVien/Views/QuanLyDanhMucPopup/CategoryPopup.xaml.cs
@@ -0,0 +1,20 @@
+﻿using QuanLyThuVien.ViewModels;
+using QuanLyThuVien.ViewModels.QuanLyDanhMuc;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace QuanLyThuVien.Views.QuanLyDanhMucPopup
+{
+    /// <summary>
+    /// Interaction logic for QuanLySachView.xaml
+    /// </summary>
+    public partial class CategoryPopup : UserControl
+    {
+        public CategoryPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
